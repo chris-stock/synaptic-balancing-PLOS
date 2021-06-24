05badf33edc56436cda5f8bf0f23837512332e9e
Chris Stock <christopher.h.stock@gmail.com>
2021-06-24 14:46:57 -0400

    initial plotting code

---
+++ calculate_imbalances_during_training.py
@@ -0,0 +1,132 @@
+import os
+import numpy as np
+import pickle as pkl
+import matplotlib.pyplot as plt
+
+####  DATA PATHS
+THIS = '07'
+data_dir = '../data/neural-gradients-regularized-training/'
+fig_dir = '../figures_new/trained-networks'
+fig_path='g-norm-regularized-training-{}.pdf'.format(THIS)
+fname = os.path.join(data_dir, 'imbalances-during-regularized-training-04.pkl')
+
+
+### LOAD DATA
+with open(fname, 'rb') as f:
+    data = pkl.load(f)
+    
+train_results = [{'weights': w} for w in data['initial_final_weights']]
+g_norm = data['g_norm']
+l2_reg_scale = data['l2_reg_scale']
+print("Loaded data: {}".format(fname))
+
+## HELPER FUNCTIONS
+
+yellow_rgb = np.array([251, 176, 59])/256.
+purple_rgb = np.array([102, 45, 145])/256.
+red_rgb = np.array([193, 39, 45])/256.
+teal_rgb = np.array([0, 169, 157])/256.
+
+
+def calc_neural_gradient(C):
+    return np.sum(C - C.T, axis=1)
+
+def calc_g_norm(J, shuffle=False, rows=True):
+    C = J**2
+    if shuffle:
+        C = np.random.permutation(C) if rows else np.random.permutation(C.T).T
+    g = calc_neural_gradient(C)
+    return np.linalg.norm(g)
+
+
+### SHUFFLE ROWS    
+n_shuff = int(1e4)
+gf_shuff_rows = [
+    [calc_g_norm(res['weights'][-1]['W_rec'], shuffle=True, rows=True)
+     for _ in range(n_shuff)]
+    for res in train_results]
+g0_shuff_rows = [
+    [calc_g_norm(res['weights'][0]['W_rec'], shuffle=True, rows=True)
+     for _ in range(n_shuff)]
+    for res in train_results]
+
+gf_true = [
+    calc_g_norm(res['weights'][-1]['W_rec'], shuffle=False)
+    for res in train_results
+]
+
+### PLOT IMBALANCES OVER LEARNING VS. SHUFFLED IMBALANCES
+figsize=(3,2.5)
+fontsize=8
+legendfontsize=6
+xticks = [0, 2000, 4000, 6000]
+yticks = [0, .5, 1, 1.5, 2, 2.5]
+linewidth=1
+line_colors = [yellow_rgb, red_rgb, purple_rgb]
+niter=6000
+extra_x=500
+hist_scale=30 
+
+fig, ax = plt.subplots(1, figsize=figsize)
+
+# for res, c in zip(train_results, line_colors):
+#     g_norm = np.linalg.norm(res['zen_imbalances'], axis=1)
+for gn, c in zip(g_norm, line_colors):
+    ax.plot(gn, c=c, linewidth=linewidth)
+for i, gf in enumerate(gf_shuff_rows):
+    counts, bins = np.histogram(gf_shuff_rows[i], density=True)
+    ax.hist(
+        bins[:-1],
+        bins,
+        weights=hist_scale*counts,
+        color=[1 - .7*(1-v) for v in line_colors[i]],
+#         density=True,
+        orientation='horizontal',
+        bottom=niter,
+    )    
+    ax.plot(
+        [niter,niter+extra_x],
+        [gf_true[i], gf_true[i]],
+        lw=linewidth,
+        c=line_colors[i],
+        linestyle=':'
+    )
+    
+ax.set_title('Network balance during training \n '
+    'with $\ell_2$ regularization ($\lambda$)', fontsize=fontsize
+)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_position(('data', 0))
+ax.spines['left'].set_position(('data', 0))
+
+
+    
+# ax.set_ylabel('Norm of neural gradient', fontsize=fontsize)
+ax.set_yticks(yticks)
+ax.set_yticklabels(yticks, fontsize=fontsize)
+
+ax.set_xlabel('Training iteration', fontsize=fontsize)
+ax.set_ylabel('Norm of neural gradients', fontsize=fontsize)
+ax.set_xticks(xticks)
+ax.set_xticklabels(xticks, fontsize=fontsize)
+
+legend_labels=['$\lambda = {}$'.format(zr) for zr in l2_reg_scale]
+legend_labels[-1] = legend_labels[-1]+' '*10
+ax.legend(
+    legend_labels,
+    fontsize=legendfontsize,
+    loc='upper left',
+    bbox_to_anchor=(.55,0,.1,.75),
+    borderaxespad=0,
+    frameon=False,  
+    ncol=1,
+)
+
+plt.tight_layout()
+plt.savefig(
+    os.path.join(fig_dir, fig_path),
+    dpi=300
+)
+
+print("Saved figure: {}".format(os.path.join(fig_dir, fig_path)))
---
+++ train_regularized_networks.py
@@ -0,0 +1,179 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import zen
+from zen import tasks, ZenRNN, zen_optimizer, sgd_optimizer
+from zen.components import orth_initializer, randn_initializer, identity_initializer, custom_initializer
+from zen.components import relu, frob_regularizer, no_regularizer
+from os.path import join
+import os
+import pickle as pkl
+train_results = None # will be defined and used later by rnn.train
+
+
+####  DATA PATHS
+THIS = '07'
+data_dir = '../data/neural-gradients-regularized-training/'
+trial_fpath = join(data_dir, 'integration_trials_{}.pkl'.format(THIS))
+fig_dir = '../figures_new/trained-networks'
+
+data_dir = '../data/neural-gradients-regularized-training/'
+data_fpath = os.path.join(data_dir, 'imbalances-during-regularized-training-{}.pkl'.format(THIS))
+
+
+
+### PLOTTING CODE
+yellow_rgb = np.array([251, 176, 59])/256.
+purple_rgb = np.array([102, 45, 145])/256.
+red_rgb = np.array([193, 39, 45])/256.
+teal_rgb = np.array([0, 169, 157])/256.
+
+def plot_performance(rnn, ntrials=24):
+    fig, axes = plt.subplots(2, 4, sharex=True, sharey=True)
+    for _ in range(ntrials):
+        result, target = rnn.simulate(trial_generator=task.generate_all_conditions)    
+        for i, ax in enumerate(axes.ravel()):
+            ax.plot(result['outputs'][i][0], c='darkblue', alpha=.7)
+            ax.plot(result['outputs'][i][1], c='orange', alpha=.7)
+            ax.set_title('target: {}'.format(np.argmax(target[i, :,-1])))
+    fig.tight_layout()
+
+
+
+### TRIAL PARAMS
+context_params = {
+    'noise': .1,
+    'signal': 1,
+    'num': 2,
+    'onset': {
+        'low': 0,
+        'high': 1
+    },
+    'offset': {
+        'low': 100,
+        'high': 101
+    }
+}
+
+input_params = {
+    'noise': .1, #1
+    'signal': 1,
+    'num': 2,    'onset': {
+        'low': 0,
+        'high': 1
+    },
+    'offset': {
+        'low': 100,
+        'high': 101
+    }
+}
+
+task_params = {
+    'num_timesteps': 50,
+    'context_params': context_params,
+    'input_params': input_params
+}
+
+n_trials = 10000 # number of trials to generate of the task
+
+
+### NETWORK & TRAINING PARAMETERS
+
+l2_reg_scale = np.array([0., .1, .3], dtype=float)
+N = 256
+
+n_iter = 60
+learning_rate = 5e-3
+
+
+### GENERATE TRIALS
+task = zen.tasks.IntegrationTask(**task_params)
+canon_inputs, canon_targets, clabels, ilabels = task.generate_all_conditions(return_labels=True)
+n_conditions = len(clabels)
+trials = [task.generate_all_conditions(return_labels=True) for _ in range(n_trials)]
+with open(trial_fpath, 'wb') as f:
+    pkl.dump(trials, f, -1)
+
+
+### TRAIN NETWORKS
+
+
+def train_with_zen_regularization(N, l2_rate, initial_weights=None, learning_rate=1e-2, niter=1600):
+    calc_regularizer = lambda rate: no_regularizer if rate==0 else frob_regularizer(l2_rate)
+    
+    # set RNN parameters
+    clipping = {
+        'method': tf.clip_by_global_norm,
+        'args': (10.0,) #(10.0,)
+    }
+    p = 2
+    
+    if initial_weights is None:
+        initializers = {
+            'W_rec': randn_initializer(stddev=2./np.sqrt(N)),
+            'W_in': randn_initializer(stddev=1/np.sqrt(N)),
+            'W_out': randn_initializer(stddev=1/np.sqrt(N)),
+            'b':  lambda s, _: np.zeros(s),                    
+            }
+    else:
+        initializers = {k: custom_initializer([w]) for k, w in initial_weights.items()}
+    
+    rnn_params = {
+        'p': p,
+        'num_neurons': N,
+        'nonlinearity': relu,
+        'zen_penalty_strength': l2_rate,    
+        'initializers': initializers
+    }
+
+    # make RNN
+    rnn = ZenRNN(task, **rnn_params)
+    initial_weights = rnn.dump_weights()
+    train_op = sgd_optimizer(rnn, clipping=clipping)
+    train_results = None
+
+    # set training parameters
+    train_args = {
+        'train_op': train_op,
+        'niter': niter,
+        'learning_rate': learning_rate,
+        'append_to': train_results,
+        'trial_generator': task.generate_all_conditions,
+        'save_weights': True,
+    }
+
+    # train network
+    train_results = rnn.train(**train_args)
+
+    return rnn, train_results
+
+initial_weights = {
+    'W_rec': randn_initializer(stddev=1./np.sqrt(N))((N,N), 0),
+    'W_in': randn_initializer(stddev=1./np.sqrt(N))((task.num_inputs,N), 0),
+    'W_out': randn_initializer(stddev=1./np.sqrt(N))((N,task.num_outputs), 0),
+    'b': np.zeros((N,))
+}    
+
+train_args = [{
+    'N': N,
+    'l2_rate': l2_reg,
+    'niter': n_iter,
+    'initial_weights': initial_weights,
+    'learning_rate': learning_rate,
+} for l2_reg in l2_reg_scale]
+
+results = [train_with_zen_regularization(**args) for args in train_args]
+rnns, train_results = zip(*results)
+
+### SAVE DATA
+save_data = {
+    'g_norm': [np.linalg.norm(res['zen_imbalances'], axis=1) for res in train_results],
+    'loss': [np.linalg.norm(res['loss']) for res in train_results],
+    'zen_cost': [np.linalg.norm(res['zen_cost']) for res in train_results],
+    'initial_final_weights': [(res['weights'][0], res['weights'][-1]) for res in train_results],    
+    'l2_reg_scale': l2_reg_scale,
+}
+
+with open(data_fpath, 'wb') as f:
+    pkl.dump(save_data, f, -1)
+
